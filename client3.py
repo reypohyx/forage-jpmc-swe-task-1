ea3a7faf3c1ab33b34bb2f1bcc79bd62ad07a25d
reypo <reypohyx@gmail.com>
2023-12-22 12:40:26 +0000

    Fixed bugs in client3.py and added unit tests in client_test.py

--- client3.py
+++ client3.py
@@ -35,14 +35,18 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if(price_b == 0):
+        # when price_b is 0 avoid throwing ZeroDivisionError
+            return None
+    # Calculate and return the ratio of price_a to price_b
+    return price_a/price_b
 
 
 # Main
@@ -52,8 +56,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            # Created a prices dictionary to store the stock prices
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+        # Used the prices dictionary at the end to pass in the right values in the getRatio function.
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,18 +9,33 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
-
-
-
+  def test_getRatio_calculateRatioValidInputs(self):
+    # Test case with valid inputs
+    result = getRatio(10,5)
+    # Comparing getRatio return value with expected value of 2.0
+    self.assertEqual(result, 2.0)
+  def test_getRatio_divisionbyZero(self):
+    # Test case to handle division by zero
+    result = getRatio(10,0)
+    # Comparing getRatio return value with expected value of None
+    self.assertEqual(result, None)
+  def test_getRatio_divisionofZero(self):
+    # Test case to handle division of zero
+    result = getRatio(0, 5)
+    # Comparing getRatio return value with expected value of 0
+    self.assertEqual(result, 0
+                     )
 if __name__ == '__main__':
     unittest.main()
